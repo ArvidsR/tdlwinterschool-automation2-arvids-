da8fa140af0dbfdaa4bdd7cec91975e67ea9f572
ArvidsR <arvidijos@gmail.com>
2024-02-22 11:26:38 +0200

    almost finished order feature

---
+++ features/test-data/users.js
@@ -0,0 +1,8 @@
+export const users = {
+    Arvids: {
+        email: "arvids.reinbergs@inbox.lv",
+        password: "Arvids123",
+        firstname: "Arvids",
+        lastname: "Reinbergs"
+    }
+};  
